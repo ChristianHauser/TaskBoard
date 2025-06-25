070bba5b1c88b4442bed55b39bf12b9f172cce79
ChristianHauser <mr.chris.chris1@gmail.com>
2025-06-25 17:53:03 +0200

    Added Firebase JWT library and implemented test version for registerphp sending a cookie

--- backend/api/register.php
+++ backend/api/register.php
@@ -1,11 +1,18 @@
 <?php
 require_once __DIR__ ."/../db/config.php";
+require_once __DIR__ ."/../loadAnyEnv.php";
+
+require __DIR__ ."/../vendor/autoload.php";
 session_start();
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: http://localhost:3000");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
+
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
@@ -42,10 +49,39 @@ if($check->fetch()){
     echo json_encode(["error"=> "Email is already registered"]);
     exit();
 }
+
 $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 $sql ="INSERT INTO user(user_name,email,password) VALUES (?,?,?)";
 $stmt = $pdo->prepare($sql);
 $stmt->execute([$user_name,$email, $hashedPassword]);
+
+//BLOCK JWT TOKEN TILL EMAIL IS VERIFIED?
+$selectUserByMail = "SELECT * FROM user WHERE email = ?";
+$checkEmailVerification = $pdo->prepare($selectUserByMail);
+$checkEmailVerification->execute([$email]);
+$user = $checkEmailVerification->fetch();
+
+if($user->email_verified){
+    $user_id = $user->id;
+
+    $payload = [
+        "sub" => $user_id,
+        "iat" => time(),
+        "exp" => time()+ 3600 * 24,
+    ];
+
+    $secretKey = getenv("TOKEN_KEY");
+
+    $jwt = JWT::encode($payload, $secretKey, ["HS256"]);
+
+    setcookie("token", $jwt, [
+        "expires" => time()+3600*24,
+        "path" => "/",
+        "httponly" => true,
+        "samesite" => "Lax",
+        "secure" => false
+    ]);
+}
 
 echo json_encode(["success"=> true,"message"=>"registered new user"]);
 exit();
