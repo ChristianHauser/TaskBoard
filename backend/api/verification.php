1d83e27cf60cec5936b7a27a9067ffae68d71754
ChristianHauser <mr.chris.chris1@gmail.com>
2025-07-02 20:51:10 +0200

    sending firebasetokens through http

--- backend/api/verification.php
+++ backend/api/verification.php
@@ -4,3 +4,19 @@ require_once __DIR__ ."../db/config.php";
 
 header("Content-Type: application/json");
 header("Access-Controll-Allow-Origin: http://localhost:3000");
+
+
+$data = json_decode(file_get_contents("php://input"));
+
+$userToken = $data->tokenId ?? "";
+
+$sql = "SELECT * FROM user WHERE token_id = ?";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([$userToken]);
+$dataBaseUserIdToken = $stmt->fetch();
+
+if($userToken || $dataBaseUserIdToken) {
+    if($dataBaseUserIdToken == $userToken) {
+        echo json_encode("user logged in");
+    }
+}
