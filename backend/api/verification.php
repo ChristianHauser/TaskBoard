920467c765950b9582ade37bee82031f3483a6d0
ChristianHauser <mr.chris.chris1@gmail.com>
2025-07-04 21:16:36 +0200

    added join select for users_of_projects table

--- backend/api/verification.php
+++ backend/api/verification.php
@@ -21,11 +21,44 @@ if(!$userToken){
     echo json_encode(["error"=> "Missing token from clientside"]);
     exit;
 }
+
+
 try{
     $verifyIdToken = $auth->verifyIdToken($userToken);
+
+    $uid = $verifiedIdToken->claims()->get('sub');
+
+    $userSql = "SELECT * FROM user WHERE firebase_uid = ?";
+    $userStmt = $pdo->prepare($userSql);
+    $userStmt->execute([$uid]);
+    $user = $userStmt->fetch();
+    
+
+    $projectSql = "SELECT p.id AS project_id, p.project_name, uop.role 
+                   FROM user u JOIN users_of_project uop ON u.id = uop.user_id
+                   JOIN projects p ON p.id = uop.project_id
+                   WHERE u.id = ?";
+                    
+    $projectStmt = $pdo->prepare($projectSql);
+    $projectStmt->execute([$user->id]);
+    $projectsFetch = $projectStmt->fetchAll(PDO::FETCH_OBJ);
+
+    
+
+    foreach( $projectsFetch as $projects){
+        echo $projects->project_name;
+        echo $projects->role;
+    }
+
+    echo json_encode(["Message" => "Token is verified", "user" => $user]);
+
 }catch(\Kreait\Firebase\Exception\AuthException $e){
-    echo json_encode(["error"=> $e->getMessage()]);
+    echo json_encode(["error"=> "Invalid token" . $e->getMessage()]);
 }
+
+
+
+
 $sql = "SELECT * FROM user WHERE token_id = ?";
 $stmt = $pdo->prepare($sql);
 $stmt->execute([$userToken]);
